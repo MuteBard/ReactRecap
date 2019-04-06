89cc702663dad6ad58651af69d4eae3860bce70a
MuteBard <csevere@brandeis.edu>
2019-04-06 11:30:10 -0400

    created commentbox and commentlist

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommentBox from './CommentBox'
+import CommentList from './CommentList'
+
+//functional component
+export default () => {
+    return (
+        <div>
+            <CommentBox/>
+            <CommentList/>
+        </div>
+    )
+}
