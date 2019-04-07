c04123b8f5334c8208a932634b5b443c59d45c8b
MuteBard <csevere@brandeis.edu>
2019-04-06 23:32:29 -0400

    added axios usage

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,10 +1,13 @@
-import { SAVE_COMMENT } from 'actions/types'
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
 
 export default function(state = [], action){
     switch (action.type){
         case SAVE_COMMENT:
         //take all of our existing comments and put it on this array and we will add on top of that the new comment that has been entered
             return [...state, action.payload];
+        case FETCH_COMMENTS:
+            const comments = action.payload.data.map(comment => comment.name)
+            return [...state, ...comments]
         default:
             return state;
     }
